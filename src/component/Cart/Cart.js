6d3995fffad6e9c176d5a50d8e812c645edeae01
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-03-23 07:03:45 +0600

    all calulation fixed

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+    // const {cart} = props;
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping
+    }
+    const tax = Number((total * 0.1).toFixed(2));
+    const grandTotal = (total + shipping + tax).toFixed(2);
+
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summary </h3>
             <p>Selected Items : {cart.length}</p>
+            <p>Total Price : ${total}</p>
+            <p>Total Shipping : ${shipping}</p>
+            <p>Tax : ${tax} </p>
+            <h5>Grand Total : ${grandTotal}</h5>
         </div>
     );
 };
